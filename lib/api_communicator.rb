44c8b67488e785d47c2bc19823e030c9a7927329
munirkizilcali <munir.kizilcali@gmail.com>
2018-04-24 15:21:57 -0400

    version munir

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,12 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  films = []
+  character_hash["results"].each do |char|
+    if char["name"].downcase == character
+      films = char["films"]
+    end
+  end
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,10 +21,24 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+  if films == []
+    puts "No character available by that name in any film."
+  end
+  films
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  movies = []
+  movie_hash = {}
+  films_hash.each do |url|
+    movie_hash = JSON.parse(RestClient.get(url))
+    puts "Movie: #{movie_hash['title']}"
+    puts "Episode: #{movie_hash['episode_id']}"
+    puts "Year: #{movie_hash['release_date']}"
+    puts "Director: #{movie_hash['director']}"
+    puts "***************************"
+  end
 end
 
 def show_character_movies(character)
@@ -31,3 +50,4 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  puts "Hello young Padawan!"
 end
 
 def get_character_from_user
   puts "please enter a character"
+  char_input = gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
