e24d3c80627bbf00f592fccb483617f6c80979aa
joshpson <jpearson846@gmail.com>
2018-04-24 15:24:09 -0400

    Updated api communicator and command line interface to take in a character name

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,8 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to jedi training school!"
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "please enter a character\n\n"
+  character = gets.chomp.downcase
 end
